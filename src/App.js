5d5c9f2892b4418ec63f60f7060d0da448123970
wangsaihu <wangsaihu@ilarge.cn>
2020-07-24 11:43:04 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import loadable from './utils/loadable'
+// import 'animate.css'
+import './style/base.scss'
+import './style/App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// 公共模块
+const DefaultLayout = loadable(() => import(/* webpackChunkName: 'default' */ './containers'))
 
-export default App;
+// 基础页面
+const View404 = loadable(() => import(/* webpackChunkName: '404' */ './views/Others/404'))
+const View500 = loadable(() => import(/* webpackChunkName: '500' */ './views/Others/500'))
+const Login = loadable(() => import(/* webpackChunkName: 'login' */ './views/Login'))
+
+const App = () => (
+    <Router>
+        <Switch>
+            <Route path='/' exact render={() => <Redirect to='/index' />} />
+            <Route path='/500' component={View500} />
+            <Route path='/login' component={Login} />
+            <Route path='/404' component={View404} />
+            <Route component={DefaultLayout} />
+        </Switch>
+    </Router>
+)
+
+export default App
